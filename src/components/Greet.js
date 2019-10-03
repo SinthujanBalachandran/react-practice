cfc75bf72bb87def40e6171606b0832014b751e1
SinthujanBalachandran <sinthujan2233@gmail.com>
2019-10-03 17:18:48 +0530

    3.10

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css'
+
+ const Greet = () => {
+    return React.createElement('div',{className:'primary'},
+    React.createElement('h1',null,'HerloSinthu'))
+}
+// const Greet = () => (
+//     <div className="primary">
+//       <p>Get started with CSS styling</p>
+//     </div>
+//   );
+
+export default Greet; 
